6500540f665b6a41f631da9e20df41b485962146
Sam Lurye <slurye@Sam-Luryes-MacBook-Pro.local>
2017-05-17 15:27:56 -0400

    Added 'caching'

--- app.js
+++ app.js
@@ -32,19 +32,40 @@ app.use(express.static('statics'));
 // set templating engine to pug
 app.set('view engine', 'pug');
 
-// load the homepage
-app.get('/', function(req, res) {
- 	res.render('index');
-});
 
-app.get('/load', function(req, res) {
-	var tweets = [];
+/*
+	The Twitter API limits you to 900 requests/15 minutes, to the last 3200 tweets/user,
+	and to 200 tweets/request. Getting 3000 tweets, then, requires 15 separate requests
+	to Twitter's servers, and therefore takes a substantial amount of time. My solution
+	is to 'cache' the most recent 3000 tweets every 15 minutes. This dramatically improves
+	client-side loading times and ensures that the site never exceeds the 900 request limit.
+
+	Note: I tried using a dedicated NodeJS caching module, but it was still really slow.
+*/
+
+// the 'cache'
+var tweets = []
+// whether or not there are tweets in the cache
+var tweetsCached = false;
+
+// put tweets into the cache 
+function cacheTweets() {
+	// temporary storage for new tweets, so the old ones can still be accessed while being updated
+	newTweets = [];
+	/* 
+		Recursively request 200 tweets at a time from the Twitter API
+		The max_id parameter allows you to set the most recent tweet you get,
+		so after the first call (which returns the most recent 200 tweets),
+		set the max_id to the earliest tweet of the last 200 returned. This allows
+		the application to go further and further back in time.
+	*/
 	function getTweets(max_id, isFirstCall) {
 		var params;
 		if (isFirstCall) {
 			params = {
 				screen_name : 'realDonaldTrump',
 				count : 200,
+				// don't include retweets
 				include_rts : false
 			};
 		} else {
@@ -55,16 +76,45 @@ app.get('/load', function(req, res) {
 				include_rts : false
 			};
 		}
+		// query the Twitter API
 		twitter.getUserTimeline(params, error, function(data) {
-			tweets = tweets.concat(JSON.parse(data));
-			if (tweets.length < 3000) {
-				getTweets(tweets[tweets.length - 1].id, false);
+			newTweets = newTweets.concat(JSON.parse(data));
+			// keep getting tweets until there are at least 3000
+			if (newTweets.length < 3000) {
+				getTweets(newTweets[newTweets.length - 1].id, false);
 			} else {
-				res.json({tweets : tweets});
+				// put the tweets in the 'cache'
+				tweetsCached = true;
+				tweets = newTweets;
+				console.log("done");
 			}
 		});
 	}
 	getTweets(0, true);
+}
+
+// cache tweets immediately once the server starts up
+cacheTweets();
+// update tweets every 15 minutes (900000 ms)
+setInterval(cacheTweets, 900000);
+
+// load the homepage
+app.get('/', function(req, res) {
+ 	res.render('index');
+});
+
+// this route is called once the page is loaded, and returns the tweets in the cache
+app.get('/load', function(req, res) {
+	// if there are tweets in the cache, send them to the client
+	// if not, try again in 100 ms
+	function loadTweets() {
+		if (tweetsCached) {
+			res.json({tweets: tweets});
+		} else {
+			setTimeout(loadTweets, 100);
+		}
+	}
+	loadTweets();
 });
 
 // set up server to listen to specified port
--- statics/js/data.js
+++ statics/js/data.js
@@ -1,13 +1,36 @@
+/**************************************************************************************
+
+Trump Tweet History
+By: Sam Lurye
+Created: May 2017
+
+Most Recent Update: May 17, 2017
+
+This file contains a majority of the client-side code for trump-tweet-history-appspot.com.
+It handles all of the graphing etc.
+
+**************************************************************************************/
+
+/* 
+	Separates each tweet into three separate pieces of info:
+		-the text of the tweet
+		-the date of the tweet, formatted in a way that Plotly.js can understand
+		-the number of likes the tweet has
+	So texts[i], datesFormatted[i], likes[i] represents the info for the i'th tweet stored
+	The Plotly.js documentation makes it clear why I use this structure
+*/
 function tweetArrays() {
 	this.texts = [];
 	this.datesFormatted = [];
 	this.likes = [];
 }
 
+// object that will hold info about all tweets, sorted by year, month, and day
 var sortedTweets = {
 	all: new tweetArrays()
 };
 
+// some useful stuff
 var monthStuff = {
 	Jan: {
 		name: 'January',
@@ -59,111 +82,13 @@ var monthStuff = {
 	}
 };
 
-var commonWords = ['','the','of','to','and','a','in','is','it','you',
-					'that','he','was','for','on','are','with','as','i','his',
-					'they','be','at','one','have','this','from','or','had','by',
-					'not','word','but','what','some','we','can','out','other','were',
-					'all','there','when','up','use','your','how','said','an','each',
-					'she','which','do','their','time','if','will','way','about','many',
-					'then','them','write','would','like','so','these','her','long','make',
-					'thing','see','him','two','has','look','more','day','could','go',
-					'come','did','number','sound','no','most','people','my','over','know',
-					'water','than','call','first','who','may','down','side','been','now',
-					'find','any','new','work','part','take','get','place','made','live',
-					'where','after','back','little','only','round','man','year','came','show',
-					'every','good','me','give','our','under','name','very','through','just',
-					'form','sentence','great','think','say','help','low','line','differ','turn',
-					'cause','much','mean','before','move','right','boy','old','too','same',
-					'tell','does','set','three','want','air','well','also','play','small',
-					'end','put','home','read','hand','port','large','spell','add','even',
-					'land','here','must','big','high','such','follow','act','why','ask',
-					'men','change','went','light','kind','off','need','house','picture','try',
-					'us','again','animal','point','mother','world','near','build','self','earth',
-					'father','head','stand','own','page','should','country','found','answer','school',
-					'grow','study','still','learn','plant','cover','food','sun','four','between',
-					'state','keep','eye','never','last','let','thought','city','tree','cross',
-					'farm','hard','start','might','story','saw','far','sea','draw','left',
-					'late','run','don\'t','while','press','close','night','real','life','few',
-					'north','open','seem','together','next','white','children','begin','got','walk',
-					'example','ease','paper','group','always','music','those','both','mark','often',
-					'letter','until','mile','river','car','feet','care','second','book','carry',
-					'took','science','eat','room','friend','began','idea','fish','mountain','stop',
-					'once','base','hear','horse','cut','sure','watch','color','face','wood',
-					'main','enough','plain','girl','usual','young','ready','above','ever','red',
-					'list','though','feel','talk','bird','soon','body','dog','family','direct',
-					'pose','leave','song','measure','door','product','black','short','numeral','class',
-					'wind','question','happen','complete','ship','area','half','rock','order','fire',
-					'south','problem','piece','told','knew','pass','since','top','whole','king',
-					'space','heard','best','hour','better','true','during','hundred','five','remember',
-					'step','early','hold','west','ground','interest','reach','fast','verb','sing',
-					'listen','six','table','travel','less','morning','ten','simple','several','vowel',
-					'toward','war','lay','against','pattern','slow','center','love','person','money',
-					'serve','appear','road','map','rain','rule','govern','pull','cold','notice',
-					'voice','unit','power','town','fine','certain','fly','fall','lead','cry',
-					'dark','machine','note','wait','plan','figure','star','box','noun','field',
-					'rest','correct','able','pound','done','beauty','drive','stood','contain','front',
-					'teach','week','final','gave','green','oh','quick','develop','ocean','warm',
-					'free','minute','strong','special','mind','behind','clear','tail','produce','fact',
-					'street','inch','multiply','nothing','course','stay','wheel','full','force','blue',
-					'object','decide','surface','deep','moon','island','foot','system','busy','test',
-					'record','boat','common','gold','possible','plane','stead','dry','wonder','laugh',
-					'thousand','ago','ran','check','game','shape','equate','hot','miss','brought',
-					'heat','snow','tire','bring','yes','distant','fill','east','paint','language',
-					'among','grand','ball','yet','wave','drop','heart','am','present','heavy',
-					'dance','engine','position','arm','wide','sail','material','size','vary','settle',
-					'speak','weight','general','ice','matter','circle','pair','include','divide','syllable',
-					'felt','perhaps','pick','sudden','count','square','reason','length','represent','art',
-					'subject','region','energy','hunt','probable','bed','brother','egg','ride','cell',
-					'believe','fraction','forest','sit','race','window','store','summer','train','sleep',
-					'prove','lone','leg','exercise','wall','catch','mount','wish','sky','board',
-					'joy','winter','sat','written','wild','instrument','kept','glass','grass','cow',
-					'job','edge','sign','visit','past','soft','fun','bright','gas','weather',
-					'month','million','bear','finish','happy','hope','flower','clothe','strange','gone',
-					'jump','baby','eight','village','meet','root','buy','raise','solve','metal',
-					'whether','push','seven','paragraph','third','shall','held','hair','describe','cook',
-					'floor','either','result','burn','hill','safe','cat','century','consider','type',
-					'law','bit','coast','copy','phrase','silent','tall','sand','soil','roll',
-					'temperature','finger','industry','value','fight','lie','beat','excite','natural','view',
-					'sense','ear','else','quite','broke','case','middle','kill','son','lake',
-					'moment','scale','loud','spring','observe','child','straight','consonant','nation','dictionary',
-					'milk','speed','method','organ','pay','age','section','dress','cloud','surprise',
-					'quiet','stone','tiny','climb','cool','design','poor','lot','experiment','bottom',
-					'key','iron','single','stick','flat','twenty','skin','smile','crease','hole',
-					'trade','melody','trip','office','receive','row','mouth','exact','symbol','die',
-					'least','trouble','shout','except','wrote','seed','tone','join','suggest','clean',
-					'break','lady','yard','rise','bad','blow','oil','blood','touch','grew',
-					'cent','mix','team','wire','cost','lost','brown','wear','garden','equal',
-					'sent','choose','fell','fit','flow','fair','bank','collect','save','control',
-					'decimal','gentle','woman','captain','practice','separate','difficult','doctor','please','protect',
-					'noon','whose','locate','ring','character','insect','caught','period','indicate','radio',
-					'spoke','atom','human','history','effect','electric','expect','crop','modern','element',
-					'hit','student','corner','party','supply','bone','rail','imagine','provide','agree',
-					'thus','capital','won\'t','chair','danger','fruit','rich','thick','soldier','process',
-					'operate','guess','necessary','sharp','wing','create','neighbor','wash','bat','rather',
-					'crowd','corn','compare','poem','string','bell','depend','meat','rub','tube',
-					'famous','dollar','stream','fear','sight','thin','triangle','planet','hurry','chief',
-					'colony','clock','mine','tie','enter','major','fresh','search','send','yellow',
-					'gun','allow','print','dead','spot','desert','suit','current','lift','rose',
-					'continue','block','chart','hat','sell','success','company','subtract','event','particular',
-					'deal','swim','term','opposite','wife','shoe','shoulder','spread','arrange','camp',
-					'invent','cotton','born','determine','quart','nine','truck','noise','level','chance',
-					'gather','shop','stretch','throw','shine','property','column','molecule','select','wrong',
-					'gray','repeat','require','broad','prepare','salt','nose','plural','anger','claim',
-					'continent','oxygen','sugar','death','pretty','skill','women','season','solution','magnet',
-					'silver','thank','branch','match','suffix','especially','fig','afraid','huge','sister',
-					'steel','discuss','forward','similar','guide','experience','score','apple','bought','led',
-					'pitch','coat','mass','card','band','rope','slip','win','dream','evening',
-					'condition','feed','tool','total','basic','smell','valley','nor','double','seat',
-					'arrive','master','track','parent','shore','division','sheet','substance','favor','connect',
-					'post','spend','chord','fat','glad','original','share','station','dad','bread',
-					'charge','proper','bar','offer','segment','slave','duck','instant','market','degree',
-					'populate','chick','dear','enemy','reply','drink','occur','support','speech','nature',
-					'range','steam','motion','path','liquid','log','meant','quotient','teeth','shell',
-					'neck', '-', '&amp;'];
-
+// makes the plot, using Plotly.js
 function makePlot(data, name, xaxis, yaxis, rangeManual) {
+	// set the title of the graph
 	$('#header').html(name);
+	// remove the loading animation
 	$('#loading').remove();
+	// set axis titles, and make sure it always displays y = 0
 	var layout = {
 		xaxis: {
 			title: xaxis
@@ -173,12 +98,19 @@ function makePlot(data, name, xaxis, yaxis, rangeManual) {
 			rangemode: 'tozero'
 		}
 	}
+	// allow for y-axis range to be set manually
 	if (rangeManual) {
 		layout.yaxis['range'] = rangeManual.yrange; 
 	}
+	// make the plot
 	Plotly.newPlot('plot', data, layout);
 }
 
+/* 
+ 	the Twitter API sends the date as a string formatted as 
+  		"<weekday> <month name abbr.> <day> <hh>:<mm>:<ss> <???> <yyyy>"
+ 	parseDate() returns an object with a tweet's date info
+*/
 function parseDate(date) {
 	var dateArr = date.split(' ');
 	var timeArr = dateArr[3].split(':');
@@ -190,6 +122,10 @@ function parseDate(date) {
 		day = dateArr[2];
 	}
 	return {
+		/* 
+		 	to plot date on the x-axis with Plotly, it needs to be formatted as
+		 	"yyyy-mm-dd hh:mm:ss"
+		 */
 		formatted: dateArr[5] + '-' + monthStuff[dateArr[1]].num + 
 					'-' + dateArr[2] + ' ' + dateArr[3],
 		year: dateArr[5],
@@ -201,11 +137,40 @@ function parseDate(date) {
 	}
 }
 
+/*
+ 	sort the tweets in a way that makes plotting easy:
+ 	{
+		all: tweetArrays object with all tweets,
+		2016: {
+			all: tweetArrays object with all tweets from 2016,
+			Jan: {
+				all: tweetArrays object with all tweets from Jan 2016,
+				days: [
+					index = 0: undefined,
+					index = 1: tweetArrays object with all tweets from Jan 1, 2016,
+					index = 2: ...,
+					.
+					.
+					. 
+				]
+			},
+			Feb: ...,
+			.
+			.
+			.
+		},
+		2017: ...,
+		.
+		.
+		.
+ 	}
+ */
 function sortTweets(tweets) {
 	for (i = 0; i < tweets.length; i++) {
 
 		var dateInfo = parseDate(tweets[i].created_at);
 
+		// if a tweet is the first in its year, month, or day, create the relevant object for storage
 		if (!sortedTweets[dateInfo.year]) {
 			sortedTweets[dateInfo.year] = {all: new tweetArrays()}
 		}
@@ -219,6 +184,7 @@ function sortTweets(tweets) {
 			sortedTweets[dateInfo.year][dateInfo.month].days[dateInfo.day] = new tweetArrays();	
 		}
 
+		// put the info where it should be
 		sortedTweets.all.texts.push(tweets[i].text);
 		sortedTweets.all.datesFormatted.push(dateInfo.formatted);
 		sortedTweets.all.likes.push(tweets[i].favorite_count);
@@ -237,6 +203,10 @@ function sortTweets(tweets) {
 	}
 	console.log(sortedTweets);
 }
+
+// ****************************** 
+// Functions for plotting likes
+// ******************************
 
 function plotAllLikes() {
 	var data = [{
@@ -270,7 +240,11 @@ function plotLikesByMonth(month, year) {
 	}];
 	makePlot(data, "Likes Per Tweet Over Time: " + month + " " + year, "Date and Time of Tweet", "Likes");
 }
+// ******************************
 
+// ******************************
+// Add the different options for plotting likes
+// ******************************
 function addEventToMonthButton(monthID) {
 	$('#' + monthID).click(function() {
 		var month = monthID.substring(0, 3);
@@ -325,7 +299,12 @@ function configureOptionsForOverTime() {
 		plotAllLikes();
 	});
 }
+// ******************************
 
+// ******************************
+// If plotChange = true, plots the change in average likes per day
+// If plotChange = false, plots the average likes per tweet over time
+// ******************************
 function plotAverageLikes(plotChange) {
 	var sum = 0;
 	var likes = sortedTweets.all.likes;
@@ -358,6 +337,9 @@ function plotAverageLikes(plotChange) {
 	}
 }
 
+// ******************************
+// Function for plotting number of tweets per day
+// ******************************
 function plotTweetsPerDay() {
 	var days = [];
 	var tweetsPerDay = [];
@@ -383,10 +365,15 @@ function plotTweetsPerDay() {
 	}];
 	makePlot(data, "Tweets Per Day", "Days", "Number of Tweets");
 }
+// ******************************
 
+// ******************************
+// Function for plotting most frequently used words
+// ******************************
 function plotCommonWords() {
 	var words = {};
 	var numMentions = {};
+	// fill an object with all words used and how many times they were used
 	for (i = 0; i < sortedTweets.all.texts.length; i++) {
 		var tweetWords = sortedTweets.all.texts[i].toLowerCase().split(' ');
 		for (j = 0; j < tweetWords.length; j++) {
@@ -397,6 +384,10 @@ function plotCommonWords() {
 			}
 		}
 	}
+	// Fill an object whose keys are number of times a word was used
+	// and whose values are arrays with all the words used that many times,
+	// getting rid of all of the most common words
+	// This automatically sorts the words in ascending order
 	for (var word in words) {
 		if ($.inArray(word, commonWords) == -1) {
 			if (!numMentions[words[word]]) {
@@ -405,8 +396,11 @@ function plotCommonWords() {
 			numMentions[words[word]].push(word);
 		}
 	}
+	// x-axis data (word)
 	var x = [];
+	// y-axis data (number of uses)
 	var y = [];
+	// get all the words used at least 50 times in descending order
 	for (var mentions in numMentions) {
 		if (mentions >= 50) {
 			for (i = 0; i < numMentions[mentions].length; i++) {
@@ -422,34 +416,42 @@ function plotCommonWords() {
 	}];
 	makePlot(data, "(Non-Common) Words Used At Least 50 Times", undefined, "Number of Uses");
 };
+// ******************************
 
+// stuff to do once the page has loaded
 $(document).ready(function() {
+	// get the most recent ~3000 tweets (limited by Twitter API)
 	$.getJSON('/load', function(response) {
         console.log(response.tweets.length);
+        // the server returns the tweets in reverse chronological order, so reverse them and then sort
         sortTweets(response.tweets.reverse());
+        // display likes over time on page load
         plotAllLikes();
         configureOptionsForOverTime();
+        // make sure each selection bar is on its own line
         $('.graph-select-bar').each(function() {
-        	$(this).css({display: 'block'});
+        	$(this).css({display: 'block'})
         });
+        // clicking back on the "Likes Over Time" option re-displays the year and month buttons
         $('#overtime').click(function() {
         	$('#graphOptionsDiv').css({display: 'block'});
         	plotAllLikes();
         });
+        // make the rest of the graph options perform their functions on click, hiding the year and month buttons
         $('#avg').click(function() {
-        	$('#graphOptionsDiv').css({display: 'none'});
+        	$('#graphOptionsDiv').css({display: 'none'})
         	plotAverageLikes(false);
         });
         $('#avgDelta').click(function() {
-        	$('#graphOptionsDiv').css({display: 'none'});
+        	$('#graphOptionsDiv').css({display: 'none'})
         	plotAverageLikes(true);	
         })
         $('#perDay').click(function() {
-        	$('#graphOptionsDiv').css({display: 'none'});
+        	$('#graphOptionsDiv').css({display: 'none'})
         	plotTweetsPerDay();
         });
         $('#uses').click(function() {
-        	$('#graphOptionsDiv').css({display: 'none'});
+        	$('#graphOptionsDiv').css({display: 'none'})
         	plotCommonWords();
         });
     });
